18b887ceed0e0b9cc303c360c7d1eb0a7c519357
ricciottati <giulio.ricci@aptiv.com>
2025-01-10 15:03:28 -0500

    small change
    test

--- ok_api.cpp
+++ ok_api.cpp
@@ -23,7 +23,7 @@ public:
     void handle_get(http_request request) {
         ucout << "Received GET request" << std::endl;
         json::value response;
-        response[U("message")] = json::value::string(U("NewUpdateAggain"));
+        response[U("message")] = json::value::string(U("NewUpdate"));
         request.reply(status_codes::OK, response);
     }
 
